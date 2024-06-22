456639ae7dff69469f33a34b58005dd1b5b76954
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-22 12:29:04 +0400

    implement updating and deleting for tasks

--- app/_lib/task-service.js
+++ app/_lib/task-service.js
@@ -32,3 +32,49 @@ export const deleteTask = async (taskId) => {
     throw error;
   }
 };
+
+export const patchTask = async (taskId, taskData) => {
+  try {
+    const patchURL = `https://x8ki-letl-twmt.n7.xano.io/api:tSDGfQun/tasks/${taskId}`;
+    const patchResponse = await fetch(patchURL, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskData),
+    });
+
+    if (!patchResponse.ok) {
+      throw new Error(`Failed to update task: ${patchResponse.statusText}`);
+    }
+
+    const updatedTask = await patchResponse.json();
+    return updatedTask;
+  } catch (error) {
+    console.error("Error updating task:", error);
+    throw error;
+  }
+};
+
+export const postTask = async (taskData) => {
+  try {
+    const postURL = "https://x8ki-letl-twmt.n7.xano.io/api:tSDGfQun/tasks";
+    const response = await fetch(postURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to add task: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error adding task:", error);
+    throw error;
+  }
+};
