402adfde6b8cc5e9b0c95c632b5e83ec8e6b83f0
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-23 13:03:23 +0400

    implement animations for sidebar and heeaders

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,20 +1,29 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import NavLink from "./NavLink";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <header className="flex justify-between w-[1000px] mx-auto py-10">
-      <h1 className=" text-lg font-bold ">
-        <Link href={"/"}>Citadeli</Link>
-      </h1>
-      <nav>
-        <ul className=" flex gap-3">
-          <NavLink route={"user"} text={"users"} />
-          <NavLink route={"task"} text={"tasks"} />
-        </ul>
-      </nav>
-    </header>
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="bg-gray-200 text-gray-700"
+    >
+      <header className="flex justify-between w-[1000px] mx-auto py-10 ">
+        <h1 className=" text-2xl font-bold  ">
+          <Link href={"/"}>Citadeli</Link>
+        </h1>
+        <nav>
+          <ul className=" flex gap-3">
+            <NavLink route={"user"} text={"users"} />
+            <NavLink route={"task"} text={"tasks"} />
+          </ul>
+        </nav>
+      </header>
+    </motion.div>
   );
 };
 
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,11 +1,18 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Main = () => {
   return (
     <section className="bg-gray-50">
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-[80vh] lg:items-start">
-        <div className="mx-auto max-w-xl text-center">
+        <motion.div
+          className="mx-auto max-w-xl text-center"
+          initial={{ y: 100 }}
+          animate={{ y: [100, -50, 0] }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
           <h1 className="text-3xl font-extrabold sm:text-5xl">
             WELCOME TO CITADEL
             <strong className="font-extrabold text-red-700 sm:block">
@@ -33,7 +40,7 @@ const Main = () => {
               Tasks
             </Link>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
