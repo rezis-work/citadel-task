fbb36baca508c0c16b4e313f8cc43fbc3947b48b
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-23 19:41:41 +0400

    implement for gender select

--- app/components/EditUserModal.js
+++ app/components/EditUserModal.js
@@ -1,6 +1,7 @@
-"use client";
 import React, { useEffect } from "react";
-import { Modal, Form, Input, Button } from "antd";
+import { Modal, Form, Input, Button, Select } from "antd";
+
+const { Option } = Select;
 
 const EditUserModal = ({ visible, user, onCancel, onSave }) => {
   const [form] = Form.useForm();
@@ -24,7 +25,7 @@ const EditUserModal = ({ visible, user, onCancel, onSave }) => {
 
   return (
     <Modal
-      visible={visible} // Corrected prop name to 'visible'
+      visible={visible}
       title="Edit User"
       onCancel={onCancel}
       footer={[
@@ -51,10 +52,21 @@ const EditUserModal = ({ visible, user, onCancel, onSave }) => {
         >
           <Input />
         </Form.Item>
-        <Form.Item name="gender" label="Gender">
-          <Input />
+        <Form.Item
+          name="gender"
+          label="Gender"
+          rules={[{ required: true, message: "Please select the gender!" }]}
+        >
+          <Select placeholder="Select gender">
+            <Option value="male">Male</Option>
+            <Option value="female">Female</Option>
+          </Select>
         </Form.Item>
-        <Form.Item name="birthday" label="Birthday">
+        <Form.Item
+          name="birthday"
+          label="Birthday"
+          rules={[{ required: true, message: "Please input the birthday!" }]}
+        >
           <Input />
         </Form.Item>
       </Form>
